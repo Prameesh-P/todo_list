e854408b39246c1ce24d0f3a7e6382b265131f83
Prameesh <prameepramee0@gmail.com>
2023-02-01 18:43:30 +0530

    almost finished

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"github.com/Prameesh-P/todo_list/controllers"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tpl *template.Template
+
+type Todo struct {
+	Item string
+	Done bool
+}
+type PageData struct {
+	Title string
+	Todos []Todo
+}
+
+func TodoList(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title: "TODO list",
+		Todos: []Todo{
+			{Item: "Learn Go  ", Done: true},
+			{Item: "Learn flutter", Done: true},
+			{Item: "Learn React", Done: true},
+		},
+	}
+	tpl.Execute(w, data)
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/todolist", controllers.TodoList)
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("/todolist", TodoList)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
